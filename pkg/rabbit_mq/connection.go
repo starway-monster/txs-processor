dc843581e5a188d36aa4a3762ec521cf3763caec
Gleb <gleb.beloded@gmail.com>
2020-05-17 13:09:57 +0300

    disable rabbitmq prefetch

--- pkg/rabbit_mq/connection.go
+++ pkg/rabbit_mq/connection.go
@@ -32,6 +32,10 @@ func connect(ctx context.Context, addr, queueName string) (<-chan amqp.Delivery,
 		return nil, err
 	}
 
+	if err := ch.Qos(0, 0, false); err != nil {
+		return nil, err
+	}
+
 	q, err := ch.QueueDeclare(
 		queueName, // name
 		true,      // durable
