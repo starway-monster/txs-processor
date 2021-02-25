513da459fbb2248abafe6d229ef1ec1f72f63e64
Raumo0 <dharapko@gmail.com>
2021-02-25 23:55:15 +0300

    Added test for IbcData to IbcStats

--- pkg/types/stats_test.go
+++ pkg/types/stats_test.go
@@ -2,6 +2,7 @@ package processor
 
 import (
     "github.com/stretchr/testify/assert"
+    "reflect"
     "testing"
     "time"
 )
@@ -44,6 +45,52 @@ func TestIbcData_Append(t *testing.T) {
         t.Run(tt.name, func(t *testing.T) {
             tt.ibcData.Append(tt.args.source, tt.args.destination, tt.args.t)
             assert.Equal(t, tt.want, tt.ibcData)
+        })
+    }
+}
+
+func TestIbcData_ToIbcStats(t *testing.T) {
+    timeArgs, _ := time.Parse("2006-01-02T15:04:05", "2006-01-02T15:04:05")
+    sourceName := "mySource"
+    destination1 := "myDestination"
+    destination2 := "myDestination2"
+    counter1 := 2
+    counter2 := 7
+    tests := []struct {
+        name     string
+        ibcData  IbcData
+        expected [][]IbcStats
+    }{
+        {
+            "IbcData(map)_to_IbcStats(slice)",
+            map[string]map[string]map[time.Time]int{sourceName: {destination1: {timeArgs: counter1}, destination2: {timeArgs: counter2}}},
+            [][]IbcStats{
+                {
+                    {sourceName, destination1, timeArgs, counter1},
+                    {sourceName, destination2, timeArgs, counter2},
+                },
+                {
+                    {sourceName, destination2, timeArgs, counter2},
+                    {sourceName, destination1, timeArgs, counter1},
+                },
+            },
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            actual := tt.ibcData.ToIbcStats()
+
+            if !reflect.DeepEqual(tt.expected[0], actual) {
+                assert.Equal(t, tt.expected[1], actual)
+            } else {
+                assert.NotEqual(t, tt.expected[1], actual)
+            }
+
+            if !reflect.DeepEqual(tt.expected[1], actual) {
+                assert.Equal(t, tt.expected[0], actual)
+            } else {
+                assert.NotEqual(t, tt.expected[0], actual)
+            }
         })
     }
 }
