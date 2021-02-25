6f4c9a7285423257849ab75d6b18b8128d01b0f2
Raumo0 <dharapko@gmail.com>
2021-02-25 21:20:48 +0300

    Added IbcData append test

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/jackc/pgx/v4 v4.6.0
 	github.com/mapofzones/cosmos-watcher v0.0.0-20200601123618-4a742185f678
 	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
+	github.com/stretchr/objx v0.2.0
+	github.com/stretchr/testify v1.5.1
 	github.com/tendermint/go-amino v0.15.1
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
 )
---
+++ pkg/types/stats_test.go
@@ -0,0 +1,43 @@
+package processor
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+    "time"
+)
+
+func TestIbcData_Append(t *testing.T) {
+    type args struct {
+        source      string
+        destination string
+        t           time.Time
+    }
+    timeArgs, _ := time.Parse("2006-01-02T15:04:05", "2006-01-02T15:04:05")
+    timeWant, _ := time.Parse("2006-01-02T15:00:00", "2006-01-02T15:00:00")
+    m := IbcData{}
+    tests := []struct {
+        name    string
+        ibcData IbcData
+        args    args
+        want IbcData
+    }{
+        {
+            "test_initial_increment",
+            m,
+            args{"mySource", "myDestination", timeArgs,},
+            map[string]map[string]map[time.Time]int{"mySource": {"myDestination": {timeWant: 1}}},
+        },
+        {
+            "test_increment_existing",
+            m,
+            args{"mySource", "myDestination", timeArgs,},
+            map[string]map[string]map[time.Time]int{"mySource": {"myDestination": {timeWant: 2}}},
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            tt.ibcData.Append(tt.args.source, tt.args.destination, tt.args.t)
+            assert.Equal(t, tt.want, tt.ibcData)
+        })
+    }
+}
